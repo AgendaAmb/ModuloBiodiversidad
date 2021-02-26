2d02b629e3c2bd7505d327dc52c36f1b6519f219
Jacob Loredo <yeicob_loredo@hotmail.com>
2021-02-26 16:39:56 -0600

    formulario base

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,233 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <title>{{ config('app.name', 'Biodiversidad') }}</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<body>
+    <div class="container-fluid justify-content-between">
+        <h2>Hoja de campo</h2>
+        <form method="POST" action="#">
+            @csrf
+            <div class="form-row m-2">
+                <div class="col-xl-6">
+                    <h2>Datos Generales</h2>
+                    <div class="form-group row g-3">
+                        <label for="FechaRecoleccion"
+                            class="col-md-4 col-form-label text-md-right">{{ __('Fecha de recoleccion de datos') }}
+                        </label>
+                        <div class="col-md-6">
+                            <input id="FechaRecoleccion" type="date" class="form-control"
+                                name="FechaRecoleccion" value="{{ old('FechaRecoleccion')}}" required autocomplete="FechaRecoleccion" autofocus>
+                        </div>
+                    </div>
 
-            .position-ref {
-                position: relative;
-            }
+                    <div class="form-group row g-3">
+                        <label for="FechaFotografia"
+                            class="col-md-4 col-form-label text-md-left">{{ __('Fecha de fotografía') }}</label>
+                        <div class="col-md-6">
+                            <input id="FechaFotografia" type="date" class="form-control"
+                                name="FechaFotografia" value="{{ old('FechaFotografia') }}" required autocomplete="FechaFotografia">
+                        </div>
+                    </div>
+                   
+                    <div class="form-group row">
+                        <label for="NombreRecolectorD" class="col-md-4 col-form-label text-md-left">{{ __('Nombre del Recolector de datos') }}</label>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+                        <div class="col-md-6">
+                            <input id="NombreRecolectorD" type="text"
+                                class="form-control @error('NombreRecolectorD') is-invalid @enderror" name="{{old('NombreRecolectorD')}}" required autocomplete="NombreRecolectorD">
+                                @error('NombreRecolectorD')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            </div>
+                    </div>
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+                    <div class="form-group row">
+                        <label for="NombreRecolectorm" class="col-md-4 col-form-label text-md-left">{{ __('Nombre del Recolector de muestra') }}</label>
+                        <div class="col-md-6">
+                            <input id="NombreRecolectorD" type="text"
+                                class="form-control @error('NombreRecolectorM') is-invalid @enderror" name="{{old('NombreRecolectorM')}}" required autocomplete="NombreRecolectorD">
+                                @error('NombreRecolectorM')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label for="NombreAutorFoto" class="col-md-4 col-form-label text-md-left">{{ __('Nombre de autor de fotografías') }}</label>
+                        <div class="col-md-6">
+                            <input id="NombreAutorFoto" type="text"
+                                class="form-control @error('NombreAutorFoto') is-invalid @enderror" name="{{old('NombreAutorFoto')}}" required autocomplete="NombreAutorFoto">
+                                @error('NombreAutorFoto')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            </div>
+                    </div>
+                    <hr>
                 </div>
-            @endif
+                
+                <div class="col-xl-6">
+                    <h2>Reconocimiento de ejemplar</h2>
+                    <div class="form-group row g-3">
+                        <label for="NombreC"
+                            class="col-md-4 col-form-label text-md-left">{{ __('Nombre Común') }}</label>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                        <div class="col-md-6">
+                            <input id="NombreC" type="text" class="form-control @error('NombreC') is-invalid @enderror"
+                                name="NombreC" value="{{ old('NombreC') }}" required autocomplete="NombreC" autofocus>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                            @error('NombreC')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row g-3">
+                        <label for="NombreCientifico"
+                            class="col-md-4 col-form-label text-md-left">{{ __('Nombre Científico') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="NombreCientifico" type="text" class="form-control @error('NombreCientifico') is-invalid @enderror"
+                                name="NombreCientifico" value="{{ old('NombreCientifico') }}" required autocomplete="NombreCientifico" autofocus>
+
+                            @error('NombreCientifico')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row g-3">
+                        <label for="NombreCientificoConf"
+                            class="col-md-4 col-form-label text-md-left">{{ __('Nombre Científico ') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="NombreCientificoConf" type="text" class="form-control @error('NombreCientificoConf') is-invalid @enderror"
+                                name="NombreCientificoConf" value="{{ old('NombreCientificoConf') }}" required autocomplete="NombreCientificoConf" autofocus
+                                data-toggle="tooltip" data-placement="top" title="(Identificación pendiente)"
+                                >
+
+                            @error('NombreCientificoConf')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row g-3">
+                        <label for="RegistroIdentificacion"
+                            class="col-md-4 col-form-label text-md-left" >{{ __('Registro del proceso de identificación') }} </label>
+
+                        <div class="col-md-6">
+                            <input id="RegistroIdentificacion" type="text" class="form-control @error('RegistroIdentificacion') is-invalid @enderror"
+                                name="RegistroIdentificacion" value="{{ old('RegistroIdentificacion') }}" required autocomplete="RegistroIdentificacion" autofocus
+                                data-toggle="tooltip" data-placement="top" title="(Institución/Nombre del identificador)">
+
+                            @error('RegistroIdentificacion')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+                </div>    
+
+                <div class="col-xl-6">
+                    <h2>Morfología y estado de fitosanitarios básicos</h2>
+                    <div class="form-group row g-3">
+                        <label for="NombreC"
+                            class="col-md-4 col-form-label text-md-left">{{ __('Nombre Común') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="NombreC" type="text" class="form-control @error('NombreC') is-invalid @enderror"
+                                name="NombreC" value="{{ old('NombreC') }}" required autocomplete="NombreC" autofocus>
+
+                            @error('NombreC')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row g-3">
+                        <label for="NombreCientifico"
+                            class="col-md-4 col-form-label text-md-left">{{ __('Nombre Científico') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="NombreCientifico" type="text" class="form-control @error('NombreCientifico') is-invalid @enderror"
+                                name="NombreCientifico" value="{{ old('NombreCientifico') }}" required autocomplete="NombreCientifico" autofocus>
+
+                            @error('NombreCientifico')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row g-3">
+                        <label for="NombreCientificoConf"
+                            class="col-md-4 col-form-label text-md-left">{{ __('Nombre Científico ') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="NombreCientificoConf" type="text" class="form-control @error('NombreCientificoConf') is-invalid @enderror"
+                                name="NombreCientificoConf" value="{{ old('NombreCientificoConf') }}" required autocomplete="NombreCientificoConf" autofocus
+                                data-toggle="tooltip" data-placement="top" title="(Identificación pendiente)"
+                                >
+
+                            @error('NombreCientificoConf')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row g-3">
+                        <label for="RegistroIdentificacion"
+                            class="col-md-4 col-form-label text-md-left" >{{ __('Registro del proceso de identificación') }} </label>
+
+                        <div class="col-md-6">
+                            <input id="RegistroIdentificacion" type="text" class="form-control @error('RegistroIdentificacion') is-invalid @enderror"
+                                name="RegistroIdentificacion" value="{{ old('RegistroIdentificacion') }}" required autocomplete="RegistroIdentificacion" autofocus
+                                data-toggle="tooltip" data-placement="top" title="(Institución/Nombre del identificador)">
+
+                            @error('RegistroIdentificacion')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+                </div>    
+
+
             </div>
-        </div>
-    </body>
+
+
+            <button type="submit" class="btn btn-primary">Sign in</button>
+
+        </form>
+    </div>
+</body>
+
 </html>
