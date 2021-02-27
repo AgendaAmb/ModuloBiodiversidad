4d5d42c729ef956479b99b4913a5e7b42a2be84b
Jacob Loredo <yeicob_loredo@hotmail.com>
2021-02-26 18:22:11 -0600

    cambio en formulario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -258,7 +258,76 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-xl-6">
+                    <h2 class="alert alert-primary text-center">Situación y entorno</h2>
+                    <div class="form-group row g-3">
+                        <label for="FechaRecoleccion"
+                            class="col-md-4 col-form-label text-md-right">{{ __('Fecha de recoleccion de datos') }}
+                        </label>
+                        <div class="col-md-6">
+                            <input id="FechaRecoleccion" type="date" class="form-control" name="FechaRecoleccion"
+                                value="{{ old('FechaRecoleccion')}}" required autocomplete="FechaRecoleccion" autofocus>
+                        </div>
+                    </div>
+
+                    <div class="form-group row g-3">
+                        <label for="FechaFotografia"
+                            class="col-md-4 col-form-label text-md-left">{{ __('Fecha de fotografía') }}</label>
+                        <div class="col-md-6">
+                            <input id="FechaFotografia" type="date" class="form-control" name="FechaFotografia"
+                                value="{{ old('FechaFotografia') }}" required autocomplete="FechaFotografia">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="NombreRecolectorD"
+                            class="col-md-4 col-form-label text-md-left">{{ __('Nombre del Recolector de datos') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="NombreRecolectorD" type="text"
+                                class="form-control @error('NombreRecolectorD') is-invalid @enderror"
+                                name="{{old('NombreRecolectorD')}}" required autocomplete="NombreRecolectorD">
+                            @error('NombreRecolectorD')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="NombreRecolectorm"
+                            class="col-md-4 col-form-label text-md-left">{{ __('Nombre del Recolector de muestra') }}</label>
+                        <div class="col-md-6">
+                            <input id="NombreRecolectorD" type="text"
+                                class="form-control @error('NombreRecolectorM') is-invalid @enderror"
+                                name="{{old('NombreRecolectorM')}}" required autocomplete="NombreRecolectorD">
+                            @error('NombreRecolectorM')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="NombreAutorFoto"
+                            class="col-md-4 col-form-label text-md-left">{{ __('Nombre de autor de fotografías') }}</label>
+                        <div class="col-md-6">
+                            <input id="NombreAutorFoto" type="text"
+                                class="form-control @error('NombreAutorFoto') is-invalid @enderror"
+                                name="{{old('NombreAutorFoto')}}" required autocomplete="NombreAutorFoto">
+                            @error('NombreAutorFoto')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <hr>
+                </div>
             </div>
+            
 
 
             <button type="submit" class="btn btn-primary text-center">Confirmar</button>
