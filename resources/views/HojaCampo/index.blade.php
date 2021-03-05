d9011c8fff312f0b3559af5b548461b228a6df51
Jacob Loredo <yeicob_loredo@hotmail.com>
2021-03-04 18:46:27 -0600

    camnios del wicho

--- resources/views/HojaCampo/index.blade.php
+++ resources/views/HojaCampo/index.blade.php
@@ -6,49 +6,24 @@
 </head>
 
 <body>
-    <div class="container-fluid justify-content-between mt-5 p-5">
+    <div class="container-fluid justify-content-between mt-5 p-5" id="appp">
         <h2 class="text-center alert alert-dark">Hoja de campo</h2>
+        
         <form method="POST" action="{{route('GHC')}}" enctype="multipart/form-data">
             @csrf
-
-            <div class="card-deck">
-                <div class="card">
-                    <img class="card-img-top" id="imagen1" src=" " alt="Card image cap">
-                    <div class="card-body">
-                        <input type="file" id="fileImg" onchange=readImage(this) />
+            <div class="row">
+                <div class="col-md-2">
+                    <div class="card" v-for="(a, index) in archivos">
+                        <img class="card-img-top" v-if="a.imagen!=''" :id="a.nombre" :src="a.imagen" alt="Card image cap">
+                        <div class="card-body">
+                            <input type="file" :id="'fileImg'+index" @change="cargarImagen($event,index)" />
+                        </div>
                     </div>
                 </div>
-                <div class="card">
-                    <img class="card-img-top" id="imagen2" src=" " alt="Card image cap">
-                    <div class="card-body">
-                        <input type="file" id="fileImg" onchange=readImage(this) />
-                    </div>
-                </div>
-
-                <div class="card">
-                    <img class="card-img-top" id="imagen2" src=" " alt="Card image cap">
-                    <div class="card-body">
-                        <input type="file" id="fileImg" onchange=readImage(this) />
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-img-top" id="imagen2" src=" " alt="Card image cap">
-                    <div class="card-body">
-                        <input type="file" id="fileImg" onchange=readImage(this) />
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-img-top" id="imagen2" src=" " alt="Card image cap">
-                    <div class="card-body">
-                        <input type="file" id="fileImg" onchange=readImage(this) />
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-img-top" id="imagen2" src=" " alt="Card image cap">
-                    <div class="card-body">
-                        <input type="file" id="fileImg" onchange=readImage(this) />
-                    </div>
-                </div>
+                
+            </div>
+            
+                
             </div>
             @include('HojaCampo.DGenerales')
             @include('HojaCampo.ReconocimientoE')
@@ -61,22 +36,47 @@
 </body>
 
 </html>
+<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+
 <script>
-    function readImage (input) {
-      
-     
+    var app = new Vue({
+  el: '#appp',
+  data: {
+    archivos:[]
+  }, 
+  mounted: function () {
+  this.$nextTick(function () {
+    this.archivos = [
+        {
+            imagen:"",
+            nombre:"Archivo1"
+            
+        },
+        {
+            imagen:"",
+            nombre:"Archivo2"
+        }
+    ]
+  })
+}, 
+methods:{
+    cargarImagen: function(e,index){
+        let t = this;
+        var input = document.getElementById('fileImg' +index);
+        console.log(input.files);
     if (input.files && input.files[0]) {
       var reader = new FileReader();
       reader.onload = function (e) {
-          
-           
-          $('#imagen2').attr('src', e.target.result); // Renderizamos la imagen
+          t.archivos[index].imagen =  e.target.result;
       }
-      const div = document.querySelector('#imagen2');
-
-// Apply style to div
-    div.setAttribute('style', 'visibility: visible');
+     
       reader.readAsDataURL(input.files[0]);
     }
+    }
+}
+
+})
+
+    function readImage (e,input) {
   }
 </script>
