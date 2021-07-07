2cbcac655b57a473a5b8ece507b427cc3071d9ce
JacobLoredo <yeicob_loredo@hotmail.com>
2021-07-07 12:07:24 -0500

    pagina para mostrar ficha tecnica terminada

--- resources/views/FichasTecnicas/indexPublic.blade.php
+++ resources/views/FichasTecnicas/indexPublic.blade.php
@@ -5,26 +5,29 @@
 <div class="container-fluid bg-secondary px-5 py-0">
     <div class="row justify-content-center">
         <div class="container-fluid bg-white mx-5 px-4 ">
-            <div class="row justify-content-center bg-warning m-4">
+            <div class="row justify-content-center  m-4" style="background-color: #e0e2e1">
                 <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <img src="{{asset('storage\Fondos\Fondo_Biodiversidad.webp')}}"
                                 class="d-block w-100 mx-auto p-5" alt="..." style="opacity: 0.8">
                             <div class="carousel-caption d-none d-sm-block" style="bottom: 43%; opacity: 1.0;">
-                                <h1 class="font-weight-bold text-uppercase" style="font-size: 50px">
+                                <h1 class="font-weight-bold text-uppercase"
+                                    style="font-size: 50px;font-family: 'Myraid Pro Bold';">
                                     {{$fichaTecnica->NombreComun}}</h1>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="container-fluid px-5 mt-5">
-                    <p class="text-center text-uppercase font-weight-bold " style="font-size: 32px;color: #466065;">
+                    <p class="text-center text-uppercase font-weight-bold "
+                        style="font-size: 32px;color: #466065;  font-family: 'Myraid Pro Bold';">
                         {{$fichaTecnica->NombreCientifico}}</p>
                     <p class="text-center text-uppercase" style="font-size:28px;color:#67817e;">Tipo de permanencia de
                         las hoja:{{$fichaTecnica->FichaTecnica->TPertenencia}}</p>
                 </div>
-                <div class="row row-cols-1 row-cols-xl-3 row-cols-lg-3 row-cols-md-3 row-cols-sm-3 especies mx-xl-5 mx-lg-4  mt-xl-5 mt-lg-4">
+                <div
+                    class="row row-cols-1 row-cols-xl-3 row-cols-lg-3 row-cols-md-3 row-cols-sm-3 especies mx-xl-5 mx-lg-4  mt-xl-5 mt-lg-4">
                     <div class="col mb-4 px-2">
                         <div class="card w-100 ">
                             <div class="card-body">
@@ -35,10 +38,9 @@
                             </div>
                             <div class="card-footer px-4 pb-4 pt-2">
                                 <p class="card-title text-center align-middle font-weight-bold mt-0"
-                                    style="font-size: 12px;">
+                                    style="color: #bcc9cb; font-size: 14px;font-family: 'Myraid Pro Bold';">
                                     FORMA DE CRECIMIENTO</p>
-                                <p class="card-title text-center align-middle font-weight-bold mt-3 "
-                                    style="font-size: 12px;">
+                                <p class="card-title text-center align-middle  mt-3 " style="font-size: 28px;">
                                     {{$fichaTecnica->FichaTecnica->Fcrecimiento}}</p>
                             </div>
                         </div>
@@ -53,7 +55,7 @@
                             </div>
                             <div class="card-footer px-4 pb-4 pt-2">
                                 <p class="card-title text-center align-middle font-weight-bold"
-                                    style="font-size: 12px;">
+                                    style="color: #bcc9cb; font-size: 14px;font-family: 'Myraid Pro Bold';">
                                     FRORACIÓN</p>
                                 <p class="card-title text-center align-middle font-weight-bold mt-3 text-break "
                                     style="font-size: 12px;">
@@ -71,8 +73,8 @@
                             </div>
                             <div class="card-footer px-4 pb-4 pt-2">
                                 <p class="card-title text-center align-middle font-weight-bold"
-                                    style="font-size: 12px;">
-                                    {{$fichaTecnica->FichaTecnica->Floracion}}</p>
+                                    style="color: #bcc9cb; font-size: 14px;font-family: 'Myraid Pro Bold';">
+                                    ORIGEN</p>
                                 <p class="card-title text-center align-middle font-weight-bold mt-3 text-break "
                                     style="font-size: 12px;">
                                     {{$fichaTecnica->FichaTecnica->Origen}}</p>
@@ -102,22 +104,78 @@
                         </div>
                     </div>
                 </div>
-               <div class="container-fluid my-5" style="font-size: 12px;color:gray;">
-                   <div class="row justify-content-around">
-                       <div class="col bg-white px-0 mx-5">
-                        <p class="card-title text-center align-middle  mt-3 text-break ">
-                            Descripción:{{$fichaTecnica->FichaTecnica->Descripcion}}</p>
-                       </div>
-                       <div class="col bg-white px-0 mx-5">
-                        <p class="card-title text-center align-middle  mt-3 text-break ">
-                            Descripción:{{$fichaTecnica->FichaTecnica->Descripcion}}</p>
-                       </div>
-                       <div class="col  bg-white px-0 mx-5">
-                        <p class="card-title text-center align-middle  mt-3 text-break ">
-                            Descripción:{{$fichaTecnica->FichaTecnica->Descripcion}}</p>
-                       </div>
-                   </div>
-               </div>
+                <div class="row row-cols-1 row-cols-xl-3 row-cols-lg-3 row-cols-md-3 row-cols-sm-3 especies mx-xl-5 mx-lg-4  mt-xl-5 mt-lg-4"
+                    style="color: gray; font-size: 14px;">
+                    <div class="col mb-4 px-2 ">
+                        <div class="col bg-white py-3" style=" height: 500px;width: 450px">
+                            <p class="card-title text-center align-middle font-weight-bolder mt-3 text-break "
+                                style="color: #bcc9cb; font-size: 14px;font-family: 'Myraid Pro Bold';">
+                                USOS</p>
+                            <p class="card-title text-center align-middle  mt-3 text-break ">
+                                {{$fichaTecnica->FichaTecnica->Usos}}</p>
+                            <p class="card-title text-center align-middle font-weight-bolder mt-3 text-break "
+                                style="color: #bcc9cb; font-size: 14px;font-family: 'Myraid Pro Bold';">
+                                CLIMA EN HÁBITAT NATURAL</p>
+                            <p class="card-title text-center align-middle  mt-3 text-break ">
+                                {{$fichaTecnica->FichaTecnica->Clima}}</p>
+                        </div>
+                    </div>
+                    <div class="col mb-4 px-2">
+                        <div class="col bg-white py-3" style=" height: 500px;width: 450px">
+                            <p class="card-title text-center align-middle font-weight-bolder mt-3 text-break "
+                                style="color: #bcc9cb; font-size: 14px;font-family: 'Myraid Pro Bold';">
+                                PORTE</p>
+                            <p class="card-title text-center align-middle  mt-3 text-break ">
+                                {{$fichaTecnica->FichaTecnica->Porte}}</p>
+                            <p class="card-title text-center align-middle font-weight-bolder mt-3 text-break "
+                                style="color: #bcc9cb; font-size: 14px;font-family: 'Myraid Pro Bold';">
+                                SISTEMA DE RAÍCES</p>
+                            <p class="card-title text-center align-middle  mt-3 text-break ">
+                                {{$fichaTecnica->FichaTecnica->Clima}}</p>
+                        </div>
+                    </div>
+                    <div class="col mb-4 px-2">
+                        <div class="col bg-white py-3" style=" height: 500px;width: 450px">
+                            <p class="card-title text-center align-middle font-weight-bolder mt-3 text-break "
+                                style="color: #bcc9cb; font-size: 14px;font-family: 'Myraid Pro Bold';">
+                                REQUERIMIENTOS DE LA ESPECIE</p>
+                            <p class="card-title text-center align-middle  mt-3 text-break ">
+                                {{$fichaTecnica->FichaTecnica->RequerimientosE}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="container-fluid  mt-4 ">
+                    <div class="row">
+                        <div class="col-12  px-0 py-5" style="background-color: #607879;height: 250px;color: white;">
+                            <p class="card-title text-center align-middle font-weight-bold"
+                                style="font-size: 24px;font-family: 'Myraid Pro Bold';">
+                                SERVICIOS AMBIENTALES</p>
+                            <p class="card-title text-center align-middle px-2 mt-3 text-break "
+                                style="font-size: 22px;">
+                                {{$fichaTecnica->FichaTecnica->ServiciosAmb}}</p>
+
+                        </div>
+
+                    </div>
+                </div>
+                <div class="container-fluid  mt-4" style="color: #607879;">
+                    <div class="row">
+                        <div class="col-12  px-5 py-5">
+                            <p class="card-title text-left align-middle font-weight-bold" style="font-size: 16px;">
+                                Amenazas y riesgos:</p>
+                            <p class="card-title text-left align-middle  mt-3 text-break  font-weight-bold "
+                                style="font-size: 14px;">
+                                {{$fichaTecnica->FichaTecnica->AmenazasRiesgos}} </p>
+                            <p class="card-title text-left align-middle font-weight-bold" style="font-size: 16px;">
+                                Amenazas y riesgos para comunidad habitables:</p>
+                            <p class="card-title text-left align-middle mt-3 text-break  font-weight-bold "
+                                style="font-size: 14px;">
+                                {{$fichaTecnica->FichaTecnica->AmenazasRiesgosHab}} </p>
+
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
