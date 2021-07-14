39abdd8b418a726eb0f31ba7d69dc3e3973b9f2e
JacobLoredo <yeicob_loredo@hotmail.com>
2021-07-14 15:51:10 -0500

    cambios esteticos de navbar

--- resources/views/Parciales/header.blade.php
+++ resources/views/Parciales/header.blade.php
@@ -1,9 +1,9 @@
-<nav  id="header">
+<nav id="header" class="p-3">
     <div>
-        <a href="#"><img src={{ asset('storage/Logos/INICIO-AGENDA-300x127.png')}} class="imagenAgenda"></a>        
+        <a href="https://ambiental.uaslp.mx/"><img src={{ asset('storage/Logos/INICIO-AGENDA-300x127.png')}} class="imagenAgenda "></a>        
     </div>
     <div>
-        <a href="#"><img src={{ asset('storage/Logos/horizontal_blanco.webp')}} class="img-fluid imagenAgenda_uaslp pt-3"></a>        
+        <a href="https://ambiental.uaslp.mx/"><img src={{ asset('storage/Logos/horizontal_blanco.webp')}} class="img-fluid imagenAgenda_uaslp pt-3"></a>        
     </div>
     <div>
         <a href="http://www.uaslp.mx"><img src={{ asset('storage/Logos/LOGO_UASLP-300x116.png')}} class="imagenUASLP"></a>
